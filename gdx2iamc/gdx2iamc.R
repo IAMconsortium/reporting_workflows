313a68e5f27e74f58a8c6ccd38f4f43be6d41dda
Laurent Drouet <ldrouet@gmail.com>
2017-04-12 11:54:24 +0200

    Update to last version of the library

--- gdx2iamc/gdx2iamc.R
+++ gdx2iamc/gdx2iamc.R
@@ -1,5 +1,4 @@
 warning("Last version of script and package is now at https://github.com/lolow/gdx2iamc")
-
 # gdx2iamc.R
 # Usage: Rscript gdx2iamc.R, or from Rstudio 
 #
@@ -18,99 +17,49 @@ warning("Last version of script and package is now at https://github.com/lolow/g
 # library(gdxrrw)
 # igdx('/opt/gams24.2/') # with your gams path
 
-# Configuration
-MODEL         = "ETAMAC"   # as defined in the registration form
-REGIONS       = c('World') # as defined in the registration form
-YEARS         = NULL       # by default look into the template
-XLSX_TEMPLATE = 'iamc_template/Diagnostics_template_2015-08-12.xlsx'
-GDX_GLOB      = '../GAMS_etamac_R/report*.gdx'
+## REQUIREMENTS
 
-# Check if working directory is source directory
-if (!is.null(parent.frame(2)$ofile)) {
-  if (getwd() != dirname(parent.frame(2)$ofile))
-    stop("please set working directory to source directory")
+if(!"gdx2iamc" %in% rownames(installed.packages())){
+  library(devtools)
+  install_github('lolow/gdx2iamc')
 }
+if(packageVersion("gdxtools")<numeric_version("0.2.0")){
+  stop("You need to install a newer version of gdx2iamc (>=0.2.0). Please run remove.packages('gdx2iamc'), restart R and rerun this script.")
+}
+library(gdx2iamc)
+library(gdxtools)
+library(data.table)
+library(stringr)
 
-source('get_libraries.R')
-source('iamc.R')
-
-# Packages
-pkgs <- c('data.table', 'stringr', 'foreach', 'openxlsx', 'tools')
-res <- lapply(pkgs, require_package)
-require_gdxtools()
-
-# Load iamc xls template
-template = iamc.template(XLSX_TEMPLATE)
-
-# Load variable definition
-iamc.vars = loadvar(template)
-
-# Define the list of gdx to processed
-gdxfiles = Sys.glob(GDX_GLOB)
-stopifnot(length(gdxfiles)>0)
-
-# Define the scenario name using the gdx name
+## PARAMETERS
+#Absolute folder where the gdx files are located
+templatedir = "iamc_template"
+templatefile = "Diagnostics_template_2015-08-12.xlsx"
+model_name = "ETAMAC"
+convert_usd =  1.10774 # Factor to convert USD2005 into USD2010
+max_row_xlsx = 15000 # Maximum number of rows in an excel file (approx 3MB)
+# NREP is case-sensitive. It should have the same case as in the registration template
+nrep.keep = c('World')
+# Convert GDX filename in scenario name
 scenario_name <- function(gdxfile){
   name = basename(gdxfile)
-  name = str_sub(name,1,-5)
+  name = str_sub(name,str_length("db_")+1,-5)
   return(name)
 }
 
-# Get gdx report variables
-gdxlist = lapply(gdxfiles, gdx)
-gdxlist.items = lapply(gdxlist,all_items)
-gdx.vars = unique(unlist(lapply(gdxlist.items, function(x) x$parameters)))
-gdx.vrep = gdx.vars[str_detect(gdx.vars,"^rep_")]
+## PROCESS
 
-# subset gdx report variables
-gdx.vrep.to.load = intersect(gdx.vrep,unique(iamc.vars$vrep))
+# Load IIASA xls template
+template = iamc.template(file.path(templatedir, templatefile))
 
-# DEBUG: variable not found
-gdx.vrep.not.found = setdiff(gdx.vrep,unique(iamc.vars$vrep))
-if(length(gdx.vrep.not.found)>0) print(paste("gdx param not associated:",paste(gdx.vrep.not.found,collapse=", ")))
-iamc.vars.not.found = setdiff(unique(iamc.vars$vrep),gdx.vrep)
-if(length(iamc.vars.not.found)>0) print(paste("gdx param not found:",paste(iamc.vars.not.found,collapse=", ")))
+# Load variable definition
+iamc.vars = load_var(template, convert_usd = usd2005_2010)
 
-# Region and year subsets
-nrep.keep = REGIONS
-if(exists("YEARS") & !is.null(YEARS)){
-  year.keep = YEARS
-} else {
-  year.keep = getyears(template)
-}
+# list of gdx
+gdxfiles = Sys.glob(file.path(templatedir, 'db*.gdx'))
 
-# load all gdx report variables
-gdx.rep = foreach(repvar=gdx.vrep.to.load)%do%{
-  print(repvar)
-  .data = rbindlist(lapply(gdxlist,extract,repvar,addgdx=T))
-  .data[,vrep:=repvar]
-  .data[,value:=as.numeric(value)]
-  .data[,year:=as.numeric(trep)]
-  .data[,trep:=NULL]
-  .data = .data[nrep %in% nrep.keep]
-  .data = .data[year %in% year.keep]
-  names(.data)[1] = 'vsub'
-  .data
-}
-gdx.rep = rbindlist(gdx.rep)
+#Load data from the GDX files and store it in gdx.data
+gdx.data = load_gdx(template, gdxfiles, nrep.keep, scen_func=scenario_name)
 
-# Identify scenario
-gdx.rep[,scenario:=scenario_name(gdx)]
-
-# alldata ! gotcha !
-iamc.vars[,vsub:=tolower(vsub)]
-iamc.vars[,vrep:=tolower(vrep)]
-gdx.rep[,vsub:=tolower(vsub)]
-gdx.rep[,vrep:=tolower(vrep)]
-alldata = merge(iamc.vars,gdx.rep,by=c("vrep","vsub"),all.x=T)
-
-# add model name
-alldata[,model:=MODEL]
-
-# DEBUG: report missing variable names
-gdx.var.avail = unique(gdx.rep[,list(vrep,vsub)])
-iamc.missing = unique(alldata[is.na(value)])
-
-# save data into xls files
-savexls(template,alldata)
-
+#Save data into xls files
+res = save_xls(template,gdx.data, iamc.vars, model_name, maxrowfile=max_row_xlsx)
